32c1eace679ab4f158def84b360c1d8fc49c8b07
satya-kr <satyajit.kr.prajapati@gmail.com>
2024-06-10 13:00:24 +0530

    add computer feature

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
-import { MessagesModule } from './messages/messages.module';
+// import { MessagesModule } from './messages/messages.module';
+import { ComputerModule } from './computer/computer.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(MessagesModule);
+  const app = await NestFactory.create(ComputerModule);
+  // const app = await NestFactory.create(MessagesModule);
+
+
+  app.enableCors({
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "*",
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
